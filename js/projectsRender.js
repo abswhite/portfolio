d376599d0f360142c957c043ff0a0854d2663f55
Abigail White <abigailwhite213@gmail.com>
2017-02-13 11:11:43 -0800

    stylign changes

--- js/projectsRender.js
+++ js/projectsRender.js
@@ -11,39 +11,26 @@ function Projects (opts) {
 }
 
 Projects.prototype.toHtml = function() {
-  // var $newProject = $('article.template').clone();
-  // $newProject.removeClass('template');
-
   var source = $('#article-template').html();
   var templateRender = Handlebars.compile(source);
   return templateRender(this);
-  //
-  // $newProject.find('a').html(this.title);
-  // $newProject.find('h4').html(this.description);
-  // $newProject.find('img').attr('src',this.imgUrl);
-  // $newProject.find('a').attr('href', this.siteUrl);
-  //
-  // $newProject.append('<hr>');
-  // return $newProject;
 };
 
 rawProjects.forEach(function(articleObject) {
   projectsArray.push(new Projects(articleObject));
-  console.log(projectsArray);
 });
 
 projectsArray.forEach(function(a) {
   $('#projects').append(a.toHtml());
 });
-
-$(window).on('hashchange', function(){
-  var hash = window.location.hash.substring(1);
-  $('section').hide();
-  $('#'+hash).show();
-  $('#site-subheading').hide();
-});
-
 //
+// $(window).on('hashchange', function(){
+//   var hash = window.location.hash.substring(1);
+//   $('section').hide();
+//   $('#'+hash).show();
+//   $('#site-subheading').hide();
+// });
+
 // function navigation() {
 //   $('main').eq(0).show();
 //   $('#home').on('click', 'a', function() {
@@ -51,3 +38,16 @@ $(window).on('hashchange', function(){
 //   });
 // }
 // navigation();
+//
+// function showPage(pageId) {
+//   $('.page').hide();
+//   $('#site-subheading').hide();
+//   $('pageId').show();
+// }
+//
+$(window).on('hashchange', function(){
+  console.log('Hash changed: ' + location.hash);
+  var hash = window.location.hash.substring(1)
+  $('.page').hide();
+  $('#'+ hash).show();
+});
