0227eef54f62423724febd335e1a877de7056e96
Abigail White <abigailwhite213@gmail.com>
2017-02-21 11:10:03 -0800

    style

--- js/projectsRender.js
+++ js/projectsRender.js
@@ -1,70 +1,72 @@
-'use strict'
-
-$(window).on('hashchange', function(){
-  console.log('Hash changed: ' + location.hash);
-  var hash = window.location.hash.substring(1)
-  $('.page').hide();
-  $('#'+ hash).show();
-});
-
-let projectView = {};
-
-function Projects (opts) {
-  this.title = opts.title;
-  this.description = opts.description;
-  this.imgUrl = opts.imgUrl;
-  this.repoUrl = opts.repoUrl;
-  this.siteUrl = opts.siteUrl;
-}
-
-Projects.all = [];
-
-Projects.prototype.toHtml = function() {
-  let template = Handlebars.compile($('#article-template').text());
-  return template(this);
-};
-
-Projects.loadAll = function(projectsData) {
-  console.log('load All');
-  projectsData.forEach(function(ele) {
-    Projects.all.push(new Projects(ele));
+'use strict';
+// (function(module){
+  $(window).on('hashchange', function(){
+    console.log('Hash changed: ' + location.hash);
+    var hash = window.location.hash.substring(1)
+    $('.page').hide();
+    $('#'+ hash).show();
   });
-  console.log(Projects.all);
-}
 
-Projects.allTitles = () => {
-  return Projects.all.map(function(el){
-    console.log('in all titles');
-    return el.title.split(' ').length;
-  })
-  .reduce(function(all, current) {
-    console.log('in all REDUCE');
-    return all + current;
-  })
-};
+  let projectView = {};
 
-projectView.projectsRender = function() {
-  console.log('render function called');
-  Projects.all.forEach(function(a) {
-    $('#projects').append(a.toHtml());
-  });
-};
-
-
-Projects.fetchAll = function() {
-  if (localStorage.projectsData) {
-    Projects.loadAll(JSON.parse(localStorage.projectsData));
-    projectView.projectsRender();
-  } else {
-    $.getJSON('/data/projectsJSON.json')
-    .then(function(projectsData) {
-      Projects.loadAll(projectsData);
-      localStorage.setItem('projectsData', JSON.stringify(projectsData));
-      projectView.projectsRender();
-      console.log('here in else');
-    })
+  function Projects (opts) {
+    this.title = opts.title;
+    this.description = opts.description;
+    this.imgUrl = opts.imgUrl;
+    this.repoUrl = opts.repoUrl;
+    this.siteUrl = opts.siteUrl;
   }
-  Projects.allTitles();
-  console.log('here in allTitles');
-  $('#projectTitle').append(Projects.allTitles());
-};
+
+  Projects.all = [];
+
+  Projects.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#article-template').text());
+    return template(this);
+  };
+
+  Projects.loadAll = function(projectsData) {
+    console.log('load All');
+    projectsData.forEach(function(ele) {
+      Projects.all.push(new Projects(ele));
+    });
+    console.log(Projects.all);
+  }
+
+  Projects.allTitles = () => {
+    return Projects.all.map(function(el){
+      console.log('in all titles');
+      return el.title.split(' ').length;
+    })
+    .reduce(function(all, current) {
+      console.log('in all REDUCE');
+      return all + current;
+    })
+  };
+
+  projectView.projectsRender = function() {
+    console.log('render function called');
+    Projects.all.forEach(function(a) {
+      $('#projects').append(a.toHtml());
+    });
+  };
+
+
+  Projects.fetchAll = function() {
+    if (localStorage.projectsData) {
+      Projects.loadAll(JSON.parse(localStorage.projectsData));
+      projectView.projectsRender();
+    } else {
+      $.getJSON('/data/projectsJSON.json')
+      .then(function(projectsData) {
+        Projects.loadAll(projectsData);
+        localStorage.setItem('projectsData', JSON.stringify(projectsData));
+        projectView.projectsRender();
+        console.log('here in else');
+      })
+    }
+    Projects.allTitles();
+    console.log('here in allTitles');
+    $('#projectTitle').append(Projects.allTitles());
+  };
+//   module.projectRender = projectRender;
+// }) (window);
